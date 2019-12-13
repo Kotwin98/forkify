bb260e5574ce12ae6f9b453fa803bd7e036ade76
Mateusz Kotwiński <mkotwinski98@gmail.com>
2019-12-13 16:52:25 +0100

    add state and controlSearch function

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,32 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResults(query){
-    try{
-        const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${query}`);   
-        console.log(res);
-    } catch(error){
-        alert(error);
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shoping list object
+ */
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = 'pizza' //TODO
+
+    if (query){
+        // 2) New search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        console.log(state.search.res)
     }
 }
-getResults('pizza');
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
