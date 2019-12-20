e67f46bddf8faef460ed30e2c8dc777a014540bf
Mateusz Kotwiński <mkotwinski98@gmail.com>
2019-12-20 11:38:16 +0100

    add deleteItem function

--- src/js/views/listView.js
+++ src/js/views/listView.js
@@ -17,3 +17,8 @@ export const renderItem = item => {
     `;
     elements.shopping.insertAdjacentHTML('beforeend', markup);
 };
+
+export const deleteItem = id => {
+    const item = document.querySelector(`[data-itemid="${id}"]`);
+    item.parentElement.removeChild(item);
+};
