f043e5608f0ab53014936d3a06eaf7dd207a6d4f
Mateusz Kotwiński <mkotwinski98@gmail.com>
2019-12-20 14:41:02 +0100

    fix bug

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import Recipe from './models/Recipe';
 import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /** Global state of the app
@@ -103,6 +104,16 @@ const controlRecipe = async () => {
 /**
  * LIST CONTROLLER
  */
+const controlList = () => {
+    // Create a new list if there is none yet
+    if(!state.list) state.list = new List();
+
+    // Add each ingredient to the list and UI
+    state.recipe.ingredients.forEach(el => {
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
+    });
+}
 
 
 // Handling recipe button clicks
@@ -117,7 +128,7 @@ elements.recipe.addEventListener('click', e => {
         // Increase button is clicked
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
-    } else if(e.target.matches('recipe__btn--add, recipe__btn--add *')){
+    } else if(e.target.matches('.recipe__btn--add, .recipe__btn--add *')){
         controlList();
     }
 });
