a15d21c4ea031b61fbc7ac19014f2a7c1739fbbc
Mateusz Kotwiński <mkotwinski98@gmail.com>
2019-12-20 16:31:18 +0100

    add isLiked method

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -13,4 +13,8 @@ export default class Likes {
         const index = this.likes.findIndex(el => el.id === id);
         this.likes.splice(index, 1);
     }
+
+    isLiked(id) {
+        return this.likes.findIndex(el => el.id === id) !== -1;
+    }
 }
