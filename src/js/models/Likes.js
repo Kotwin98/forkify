26a562374ea1f5262d3f24334aa587fd9c947c37
Mateusz Kotwiński <mkotwinski98@gmail.com>
2019-12-21 10:41:14 +0100

    add persistData function

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -21,4 +21,8 @@ export default class Likes {
     getNumLikes() {
         return this.likes.length;
     }
+
+    persistData() {
+        localStorage.setItem('likes', JSON.stringify(this.likes));
+    }
 }
