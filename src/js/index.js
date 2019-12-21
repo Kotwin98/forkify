103e8fe4f97a87a44b03fc1be1e9aa585a71ad93
Mateusz Kotwiński <mkotwinski98@gmail.com>
2019-12-21 11:20:06 +0100

    clean up the code

--- src/js/index.js
+++ src/js/index.js
@@ -67,7 +67,6 @@ elements.searchResPages.addEventListener('click', e => {
 const controlRecipe = async () => {
     // Get ID from url
     const id = window.location.hash.replace('#', '');
-    console.log(id);
 
     if(id){
         // Prepare UI for changes
@@ -213,5 +212,3 @@ elements.recipe.addEventListener('click', e => {
         controlLike();
     }
 });
-
-window.l = new List();
